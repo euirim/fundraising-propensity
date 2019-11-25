32c16cfe0b94e9e3fac1000c2d0c63dac3d866cf
Anthony Li <antli.inbox@gmail.com>
2019-11-24 22:12:50 -0800

    Modified read script to output csv, added doc2vec train script

--- csv_reader/read.py
+++ csv_reader/read.py
@@ -3,31 +3,40 @@ import datetime
 import pickle
 import spacy
 import sys
+import os
 from ds import *
 
-start_range, end_range = int(sys.argv[1]), int(sys.argv[2])
+start_range, end_range = int(sys.argv[1]), int(sys.argv[2]) # Set both to -1 to read full file
+skip_completed_campaigns = True # Don't read campaigns that have unknown goal due to being completed. If set to False, goal is set to 0.
+output_documents = False # If True, outputs .txt file of each story and title to data/documents
+
 with open(sys.argv[3], 'rb') as pickle_file:
     categories = pickle.load(pickle_file)
 
 num_categories = 0
 dataset = []
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_lg')
 
-with open('out.csv') as csv_file:
+out_file = open('dataset_' + str(start_range) + '_' + str(end_range) + '.csv','w')
+out_writer = csv.writer(out_file, delimiter=',')
+
+with open('data/out_full2.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_counter = 0
     num_rows = sum(1 for row in csv_reader)
     csv_file.seek(0)
-    for row in csv_reader:
+    for rownum, row in enumerate(csv_reader):
         if line_counter % 100 == 0:
             print ('{}/{}'.format(line_counter, num_rows))
 
-        if line_counter == 0 or line_counter < start_range:
+        if line_counter == 0 or (start_range != -1 and line_counter < start_range):
             line_counter += 1
             continue
-        if line_counter == end_range:
+        if end_range != -1 and line_counter == end_range:
             break
-        title, story, created, raised, goal, category, finished = row
+        url,title,story,created,raised,goal,category,finished,first_cover_image,story_images = row
+        #title, story, created, raised, goal, category, finished = row
+         
 
         title = nlp(title)
         story = nlp(story)
@@ -38,6 +47,7 @@ with open('out.csv') as csv_file:
         raised = int(raised)
 
         if len(goal) == 0:
+            if skip_completed_campaigns: continue
             goal = 0
         else:
             goal = int(goal)
@@ -46,9 +56,29 @@ with open('out.csv') as csv_file:
 
         finished = int(finished)
 
-        dataset.append(Data(Features(title, story, created, goal, category, finished), raised))
+        if output_documents:
+            print(story,  file=open(os.path.join('data','documents','story',str(rownum)+'.txt'), 'w'))
+            print(title,  file=open(os.path.join('data','documents','title',str(rownum)+'.txt'), 'w'))
+
+        out_row = [url,str(title).replace("\n"," "),str(story).replace("\n"," "),created,goal,category,finished,first_cover_image,story_images]
+        out_row.extend(title.vector)
+        out_row.extend(story.vector)
+        out_row.append(raised)
+
+        if line_counter == 1:
+            headings = ["url","title","story","created","goal","category","finished","first_cover_image","story_images"]
+            headings.extend(["title_vec_{}".format(i) for i in range(0, len(title.vector))])
+            headings.extend(["story_vec_{}".format(i) for i in range(0, len(story.vector))])
+            headings.append("raised")
+            out_writer.writerow(headings)
+
+        out_writer.writerow(out_row)
+
+
+        # dataset.append(Data(Features(title, story, created, goal, category, finished), raised))
 
         line_counter += 1
 
-with open('dataset_' + str(start_range) + '_' + str(end_range) + '.pkl', 'wb') as pickle_file:
-    pickle.dump(dataset, pickle_file)
+out_file.close()
+#with open('dataset_' + str(start_range) + '_' + str(end_range) + '.pkl', 'wb') as pickle_file:
+#    pickle.dump(dataset, pickle_file)
---
+++ csv_reader/train_doc2vec_model.py
@@ -0,0 +1,46 @@
+# https://medium.com/@klintcho/doc2vec-tutorial-using-gensim-ab3ac03d3a1
+'''
+This script trains a doc2vec model from a folder of .txt files.
+'''
+
+
+from os import listdir
+import os
+from os.path import join, isfile
+import gensim
+from gensim.models.doc2vec import LabeledSentence
+from os import listdir
+from os.path import isfile, join
+import gensim
+
+
+docLabels = []
+docLabels = [f for f in listdir(os.path.join('data','documents','story')) if f.endswith('.txt')]
+
+data = []
+for doc in docLabels:
+    with open(os.path.join('data','documents','story', doc), 'r') as file:
+        contents = file.read()
+        data.append(contents)
+
+
+class DocIterator(object):
+    def __init__(self, doc_list, labels_list):
+       self.labels_list = labels_list
+       self.doc_list = doc_list
+    def __iter__(self):
+        for idx, doc in enumerate(self.doc_list):
+            yield LabeledSentence(words=doc.split(),tags=[self.labels_list[idx]])
+
+it = DocIterator(data, docLabels)
+
+print("Initializing model...")
+model = gensim.models.Doc2Vec(size=300, window=10, min_count=5, workers=11,alpha=0.025, min_alpha=0.025) # use fixed learning rate
+print("Building vocab...")
+model.build_vocab(it)
+print("Training model...")
+model.train(it, total_examples=model.corpus_count, epochs=model.epochs, report_delay=5)
+print("Saving model...")
+model.save(os.path.join('data','story_doc2vec.model'))
+
+
