c36d72d70efa293a96d21717c2f20fee7ef517c2
Euirim Choi <euirim@me.com>
2019-12-07 17:50:59 -0800

    Lower memory usage

--- image_captioning/main.py
+++ image_captioning/main.py
@@ -14,26 +14,30 @@ def get_filename_from_url(url):
 
     return url.strip().split('/')[-1]
 
+def get_image_urls():
+    IMAGE_URLS_OUTPUT = "./data/image_urls.txt"
+    CHUNK_SIZE = 100000
+    for i, chunk in enumerate(pd.read_csv(CSV_FILENAME, header=0, chunksize=CHUNK_SIZE)):
+        print "Processing starting at row %d" % (i * CHUNK_SIZE)
+        # get all image URLs and save to text file
+        cover_image_urls = [url for url in chunk['first_cover_image'].tolist() if str(url) != 'nan']
+        story_image_urls = [url for url in chunk['story_images'].tolist() if str(url) != 'nan']
+        story_image_urls = ','.join(story_image_urls).split(',')
+        image_urls = cover_image_urls + story_image_urls
+
+        read_mode = "a"
+        if i == 0:
+            read_mode = "w"
+
+        with open(IMAGE_URLS_OUTPUT, read_mode) as f:
+            for url in image_urls:
+                f.write(url + '\n')
 
 def main():
-    print "Loading CSV..."
-    df = pd.read_csv(CSV_FILENAME, header=0)
-    # df = df[int(sys.argv[1]):min(int(sys.argv[1]) + 100000, df.shape[0])]
-    IMAGE_URLS_OUTPUT = "./data/image_urls.txt"
-
-    # get all image URLs and save to text file
-    print "Getting cover image URLs."
-    cover_image_urls = [url for url in df['first_cover_image'].tolist() if str(url) != 'nan']
-    print "Getting story image URLs."
-    story_image_urls = [url for url in df['story_images'].tolist() if str(url) != 'nan']
-    story_image_urls = ','.join(story_image_urls).split(',')
-    print "Accumulating image URLs."
-    image_urls = cover_image_urls + story_image_urls
-    print "Saving image URLs."
-    with open(IMAGE_URLS_OUTPUT, "w") as f:
-        for url in image_urls:
-            f.write(url + '\n')
-
+    if len(sys.argv) > 1 and sys.argv[1] != "--skip-image-urls":
+        print "Getting Image URLs"
+        get_image_urls()
+    
     # get captions for each image URL and save to text file
     print "Calling inference script."
     call('./run.sh', shell=True)
